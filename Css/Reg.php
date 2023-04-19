0dbc8549c0241fbe78fcc824b7e8c0864d9762fc
Mit17092001 <82253557+Mit17092001@users.noreply.github.com>
2023-04-19 15:52:22 +0530

    Add files via upload

--- Css/Reg.php
+++ Css/Reg.php
@@ -3,91 +3,10 @@
     <link href="app.css" rel="stylesheet">
     <link href="back.css" rel="stylesheet">
     
-        <!-- <style>
-            h1
-            {
-                text-align: center;
-                color: MediumSeaGreen;
-                margin-top:10px;
-            }            
-            .body{
-                margin:auto;
-                margin-top:30px;
-                border-style:ridge;
-                border-color:#fff;
-                border-width:10px;
-                padding-left:20px;
-                padding-right:20px;
-                background-color: rgba(20,20,20,0.8);
-                color:rgb(255,6,140);
-                width:300px;
-            }
-            .field
-            {
-                
-                width:100%;
-                height:50px;
-                margin-bottom:15px;
-               
-            }
-            .field input, .field button
-            {   
-                height: 100%;
-                width: 100%;
-                outline: none;
-                padding-left: 15px;
-                border-radius: 5px;
-                border: 1px solid lightgrey;
-                border-bottom-width: 2px;
-                font-size: 15px;
-              
-            }
-            .sub
-            {
-                width:100%;
-                height:50px;
-                margin-bottom:15px;
-               
-            }
-            .sub input
-            {
-                height: 100%;
-                width: 100%;
-                outline: none;
-                padding-left: 15px;
-                border-radius: 5px;
-                border: 1px solid MediumSeaGreen;
-                border-bottom-width: 2px;
-                font-size: 15px;
-                background-color: MediumSeaGreen;
-            }
-            body{
-              font-family: Arial,Helvetica,sans-serif;
-              background-image: url("circle.jpg");
-              background-repeat: no-repeat;
-            }
-           
-            .php{
-              padding-top : 5px;
-              color: #fff;
-              text-shadow:1px 1px red;
-              font-size: 15px;
-            }
-            .field select, .field option
-            {
-                height: 100%;
-                width: 100%;
-                outline: none;
-                padding-left: 15px;
-                border-radius: 5px;
-                border: 1px solid lightgray;
-                border-bottom-width: 2px;
-                font-size: 15px;
-                
-            }
-        </style> -->
+        
     </head>
     <body class="reg">
+        
         <div class="body">
             <h1>Sign-UP Form</h1>
             <form method="post" action="">
@@ -108,40 +27,60 @@
                 </div>
             </form>
             <div class="php">
-      <?php
-      include "conf.php";
-      if (isset($_POST['submit'])) 
-      {
-          $name=$_POST['name'];
-          $email = $_POST['email'];
+        <?php
+        include "conf.php";
+       
+function sendmail($email,$vcode)
+    {        
+        $receiver = $email;
+        $subject = "Email verification";
+        // $body = "Hi, Hwt we are happy to tell you that we have started our website kindly join with us for your comfort";
+        $body = "Hi, there to verify your mail click here <a href='http://localhost/php/Css/verify.php?email=$email & vcode=$vcode'>VERIFY</a>";
+        $sender = "From:homeservice.pme@gmail.com";
+        $sender  .= 'MIME-Version: Homeservice' . "\r\n";
+        $sender  .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-          $password = $_POST['password'];
-
-          $role=$_POST['role'];
-          $hql="SELECT * from `user` where `e-mail`='$email';";
-          $op=mysqli_query($conn,$hql);
-
-          if(mysqli_num_rows($op)==0)
-          {
-            $sql = "INSERT INTO `user`(`name`,  `e-mail`, `password`,`Role` ) VALUES ('$name','$email','$password','$role')";
-
-            $result = $conn->query($sql);
-      //$r=mysqli_fetch_assoc($result);
-          
-
-            if ($result == TRUE) 
-            {
-              echo "New record created successfully.";
-            }
-
-            $conn->close(); 
-          }
-          else
-          {
-            echo "<br>";
-            echo "Email already exist. Go to login page.";
-          }
+        if(mail($receiver, $subject, $body, $sender))
+        {
+            echo "Email sent successfully to $receiver";
         }
+        else
+        {
+            echo "Sorry, failed while sending mail!";
+        }
+    }
+        
+        if (isset($_POST['submit'])) 
+        {
+            $name=$_POST['name'];
+            $vcode=bin2hex(random_bytes(16));
+            $email = $_POST['email'];
+
+            $password =password_hash( $_POST['password'],PASSWORD_BCRYPT);
+
+            $role=$_POST['role'];
+            $hql="SELECT * from `user` where `e-mail`='$email';";
+            $op=mysqli_query($conn,$hql);
+
+            if(mysqli_num_rows($op)==0)
+            {
+                $sql = "INSERT INTO `user`(`name`,  `e-mail`, `password`,`Role`,`vcode`) VALUES ('$name','$email','$password','$role','$vcode')";
+
+                
+
+                if (mysqli_query($conn,$sql) && sendmail($email,$vcode)) 
+                {
+                echo "New record created successfully.";
+                }
+
+                $conn->close(); 
+            }
+            else
+            {
+                echo "<br>";
+                echo "Email already exist. Go to login page.";
+            }
+            }
         ?>
         </div>
             <h3>**If already registered</h3>
